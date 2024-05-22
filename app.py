aafe4d3ee67500f12a696d97139cb74b9a1508c1
shfergu <shfergu@microsoft.com>
2024-05-22 16:23:56 -0500

    extended flask app

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask!"
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        case_number = request.form['case_number']
+        reason = request.form['reason']
+        more_tickets = 'more_tickets' in request.form
+        # For now, just print these values to the console
+        print(f'Case Number: {case_number}, Reason: {reason}, More Tickets: {more_tickets}')
+        return redirect(url_for('index'))
+    return render_template('submit.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
